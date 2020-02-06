4f4bf8ed6226011d9cd99d44d12bf74fac8788c4
kumawatvishnu <kumawat.vishnu01@gmail.com>
2020-02-07 00:26:15 +0530

    First commit for ZigZag game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+    public bool gameOver;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void GameStart()
+    {
+        UIManager.instance.GameStart();
+        ScoreManager.instance.StartScore();
+        GameObject.Find("PlatformSpawner").GetComponent<PlatformSpawner>().StartPfSpawning();
+    }
+    public void GameOver()
+    {
+        gameOver = true;
+        UIManager.instance.GameOver();
+        ScoreManager.instance.StopScore();
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public static UIManager instance;
+
+    public GameObject zigzagPanel;
+    public GameObject gameOverPanel;
+    public GameObject tapText;
+    public Text score;
+    public Text highScore1;
+    public Text highScore2;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        highScore1.text = "High Score: "+PlayerPrefs.GetInt("highScore").ToString();
+    }
+
+    public void GameStart()
+    {
+        tapText.GetComponent<Animator>().Play("textDown");
+        zigzagPanel.GetComponent<Animator>().Play("panelUp");
+        //tapText.SetActive(false);
+    }
+    public void GameOver()
+    {
+        score.text = PlayerPrefs.GetInt("score").ToString();
+        highScore2.text = PlayerPrefs.GetInt("highScore").ToString();
+        gameOverPanel.SetActive(true);
+    }
+	public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
